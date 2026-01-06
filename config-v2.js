3aa28ba31ef73b27e4275cc4e93b81c297eaf038
MC-Error <mcerror310@gmail.com>
2026-01-06 16:00:10 +0530

    Update configuration with actual values
    Updated configuration values for DATABASE, BOT_NUMBER, and SESSION_ID.

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const DATABASE = "" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const BOT_NUMBER = "94756600911" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const SESSION_ID = "PRABATH-MD~tLHYlQXNw5Wb41i" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
